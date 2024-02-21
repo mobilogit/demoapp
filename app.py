3c952492eb5fcbf237af442ca1a1123e32aacb37
mobilogit <153889024+mobilogit@users.noreply.github.com>
2024-02-21 08:22:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 import time
 from math import sqrt
 
-
+app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
 
